e79f369ddd74e09e75722cb97fc47f00c6753ce2
jkim3442 <jkim3442@cs104u-5g.acad.fullerton.edu>
2019-09-07 14:12:14 -0700

    Finished Project.

--- main.cpp
+++ main.cpp
@@ -2,28 +2,44 @@
 // This program calculates and displays business expenses.
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 
 int main()
 {
-  int trip_location, days;
+  int days;
   double hotel_expense, meal_expense, total;
+  std::string trip_location;
 
   //Write intrduction and questions.
-  std::cout<< "Welcome to the Business Trip Tracker!\n";
-  std::cout<< "What is the business trip location?\n";
-  std::cin>> trip_location;
-  std::cout<< "How many days will the trip take?\n";
-  std::cin>> days;
-  std::cout<< "What is the total hotel expense?\n";
-  std::cin>> hotel_expense;
-  std::cout<< "What is the total meal expense?\n";
-  std::cin>> meal_expense;
-  
+  std::cout << "Welcome to the Business Trip Tracker!\n";
+  std::cout << "What is the business trip location? ";
+  std::cin >> trip_location;
+  std::cout << "How many days will the trip take? ";
+  std::cin >> days;
+  std::cout << "What is the total hotel expense? $";
+  std::cin >> hotel_expense;
+  std::cout << "What is the total meal expense? $";
+  std::cin >> meal_expense;
+
 
   //Calculate the tip location, days, and total cost.
 
   total = hotel_expense + meal_expense;
 
+  //Print the header row and results.
+
+  std::cout << std::setw(12) << "Location"
+    << std::setw(8) << "Days"
+    << std::setw(9) << "Hotel"
+    << std::setw(8) << "Meal"
+    << std::setw(9) << "Total" << std::endl;
+
+  std::cout << std::setw(12) << trip_location
+    << std::setw(8) << days
+    << std::setw(6) << "$" << hotel_expense
+    << std::setw(6) << "$" << meal_expense
+    << std::setw(5) << "$" << total << std::endl;
 
   return 0;
 }
